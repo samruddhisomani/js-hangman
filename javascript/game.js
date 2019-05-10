632446fb0b971dbc87a9f9badb48a24c6c267bca
Samruddhi Somani <ssomani@homeaway.com>
2019-05-10 00:11:41 -0500

    works except wins calculator

--- javascript/game.js
+++ javascript/game.js
@@ -17,6 +17,13 @@ const updateGuessesRemaining = () => {
     }
 }
 
+const updateLettersGuessed = (letter) => {
+    {
+        const para = document.createElement("p")
+        para.innerHTML = letter;
+        document.querySelector('.container__lettersGuessed').append(para);
+    }
+}
 //the first piece I need is an array of words.
 const words = ['otter', 'hippo', 'whale', 'dolphin', 'dugong', 'porpoise', 'platypus'];
 
@@ -41,8 +48,7 @@ const newWord = (words) => {
 
 let { randomWord, letters, guesses } = newWord(words);
 
-console.log(randomWord)
-
+//I need to make as many blanks as I have letters
 letters.map(letter => {
     const para = document.createElement("p")
     para.setAttribute('class', 'container__slotsElement')
@@ -53,21 +59,30 @@ letters.map(letter => {
 
 updateGuessesRemaining()
 
+//on keypress I need to do stuff
 const processGuess = event => {
     const guess = event.key;
 
-    //handling unexpected inputs
-    if (!guess.match('[a-z]')) {
+    if (!guess.match('^[a-z]$')) {
+        //handling unexpected inputs
         alert('Not a letter');
+
     } else if (!letters.includes(guess)) {
+        //handling wrong guesses
         guessesRemaining -= 1;
+        updateLettersGuessed(guess);
         updateGuessesRemaining();
+
     } else if (letters.includes(guess)) {
+        //handling correct guesses
         [...document.querySelectorAll('.container__slotsElement')].map((x, i) => {
             if (letters[i] == guess) {
                 x.innerHTML = guess;
             }
         })
+
+        updateLettersGuessed(guess);
+
     } else {
         throw ('System Error')
     }
